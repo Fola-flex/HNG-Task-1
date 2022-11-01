62e3e6da2faa2357d47cb4f659620547300c87a2
Fola-flex <91006932+Fola-flex@users.noreply.github.com>
2022-11-01 14:11:22 +0100

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='app'>
+      <header className="profile-avatar">
+        <img id="profile__img" src="/profile__img.svg" alt="profile image" />
+        <img id="share_btn" src="/_Avatar share button.svg" alt="share button icon" />
+        <h2>Annette Black</h2>
       </header>
+      <div className="link-section">
+        <div className="link-buttons">
+          <button>
+            <a href="https://mobile.twitter.com/Man_like_Fola"  id="twitter">Twitter Link</a>
+          </button>
+          <button>
+            <a href="https://training.zuri.team/"  id="btn__zuri">Zuri Team</a>
+          </button>
+          <button>
+            <a href="http://books.zuri.team" id="books">Zuri Books</a>
+          </button>
+          <button>
+            <a href="https://books.zuri.team/python-for-beginners?ref_id=<MuizAfolarinOlaore>"  id="book__python">Python Books</a>
+          </button>
+          <button>
+            <a href="https://background.zuri.team/"  id="pitch">Background check for Coders</a>
+          </button>
+          <button>
+            <a href="https://books.zuri.team/design-rules"  id="book__design">Design Books</a>
+          </button>
+          <button>
+            <a href="https://books.zuri.team/design-rules"  id="slack">Muiz Afolarin Olaore</a>
+          </button>
+        </div>
+      </div>
+      <div className='social_links'>
+        <div className="wrapper">
+          <img src="slack.svg" />
+          <img src="Icon.svg" />
+        </div>
+
+      </div>
+      <footer className="footer">
+        <img  src="/Zuri.Internship_Logo.svg" alt="Zuri Internship Logo" />
+        <img  src="/Footer text.svg" alt="HNG Internship Text" />
+        <img  src="I4G.svg" alt="I4G Logo" />
+      </footer>
+
     </div>
   );
 }
