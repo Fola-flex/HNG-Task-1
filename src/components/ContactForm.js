de3d3527b1de1d821c60ac6e68d6a2eb578e7909
Fola-flex <91006932+Fola-flex@users.noreply.github.com>
2022-11-06 05:21:44 +0100

    fix: images not loading

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -35,7 +35,7 @@ class ContactForm  extends Component {
 
     handleMessageChange = (event) => {
         this.setState({
-            firstname: event.target.value
+            message: event.target.value
         })
     }
 
@@ -55,23 +55,23 @@ class ContactForm  extends Component {
                 <h1>Contact Me</h1>
                 <p>Hi there, contact me to ask about anything you have in mind</p>
                 <div className='form-control'>
-                    <label>First Name:</label>
+                    <label>First Name</label>
                     <input type="text" id='first_name' value={this.state.firstname} onChange={this.handleFirstnameChange} placeholder="Enter your first name" required/>
                 </div>
                 <div className='form-control'>
-                    <label>Last Name:</label>
+                    <label>Last Name</label>
                     <input type="text" id='last_name' value={this.state.lastname} onChange={this.handleLastnameChange} placeholder="Enter your last name" required/>
                 </div>
                 <div className='form-control'>
-                    <label>Email:</label>
+                    <label>Email</label>
                     <input type="email" id='email' value={this.state.email} placeholder="yourname@email.com" onChange={this.handleEmailChange} required/>
                 </div>
                 <div className='form-control'>
-                    <label>Message:</label>
-                    <textarea type="text" id='message' value={this.state.message} onChange={this.handleMessageChange}placeholder="Send me a message and I'll reply as soon as possible" required></textarea>
+                    <label>Message</label>
+                    <textarea type="text" id='message' value={this.state.message} onChange={this.handleMessageChange} placeholder="Send me a message and I'll reply as soon as possible" required></textarea>
                 </div>
                 <div className='form-control'>
-                    <input type="radio" id='check' value={this.state.check} onChange={this.handleCheckChange} required/>
+                    <input type="checkbox" id='check' value={this.state.check} onChange={this.handleCheckChange} required/>
                     <label>You agree to providing your data to name who may contact you</label>
                 </div>
                 <button type='submit' id='btn__submit'>Send Message</button>
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,9 +4,9 @@ export default class Footer extends Component {
   render() {
     return (
         <footer className="footer">
-            <img  src="Zuri.Internship_Logo.svg" alt="Zuri Internship Logo" />
-            <img  src="Footer text.svg" alt="HNG Internship Text" />
-            <img  src="I4G.svg" alt="I4G Logo" />
+            <img  src="/Zuri.Internship_Logo.svg" alt="Zuri Internship Logo" />
+            <img  src="/Footer text.svg" alt="HNG Internship Text" />
+            <img  src="/I4G.svg" alt="I4G Logo" />
         </footer>
     )
   }
