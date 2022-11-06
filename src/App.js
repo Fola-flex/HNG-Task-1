8f2d3547b23cb9bd8d9432f77e7327c5a5d5f2b3
Fola-flex <91006932+Fola-flex@users.noreply.github.com>
2022-11-06 07:09:21 +0100

    fix: image fix

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './components/Home';
 import ContactForm from './components/ContactForm';
 
@@ -7,7 +7,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/HNG-Task-1' element={<Home />}></Route>
+        <Route path='/' element={<Home />}></Route>
         <Route path="/contact" element={<ContactForm />}></Route>
       </Routes>
     </Router>
